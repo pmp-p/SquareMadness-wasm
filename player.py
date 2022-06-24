beb60d3ab18853445ff691197b15a9b870aff1c0
N0thing13 <99495809+N0thing13@users.noreply.github.com>
2022-06-24 15:52:46 +0200

    Update player.py

--- player.py
+++ player.py
@@ -14,32 +14,32 @@ class Player:
     for item in items:
       pygame.draw.rect(screen, (0,255,0), item)
     
-  def move(self,items):
+  def move(self,items,w,h):
     keys = pygame.key.get_pressed()
     
     if keys[pygame.K_w]:
-      if self.rect.top > 50:
+      if self.rect.top > 75:
         self.rect.y -= self.speed
       else:
         for item in items:
           item.y += self.speed
           
     elif keys[pygame.K_s]:
-      if self.rect.bottom < 450:
+      if self.rect.bottom < h - 75:
         self.rect.y += self.speed
       else:
         for item in items:
           item.y -= self.speed
       
     if keys[pygame.K_a]:
-      if self.rect.left > 50:
+      if self.rect.left > 75:
         self.rect.x -= self.speed
       else:
         for item in items:
           item.x += self.speed
           
     elif keys[pygame.K_d]:
-      if self.rect.right < 450:
+      if self.rect.right < w - 75:
         self.rect.x += self.speed
       else:
         for item in items:
